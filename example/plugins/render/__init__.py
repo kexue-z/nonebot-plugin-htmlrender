327f018182e5d23c66718b3160e9d57ba5cc8384
kexue <xana278@qq.com>
2021-12-22 13:27:29 +0800

    🎉0.0.1

---
+++ example/plugins/render/__init__.py
@@ -0,0 +1,98 @@
+import io
+
+from nonebot import on_command
+from nonebot.adapters.cqhttp import Bot, MessageEvent, MessageSegment
+from nonebot.plugin import require
+from PIL import Image
+
+# 纯文本转图片
+text2pic = on_command("text2pic")
+text_to_pic = require("nonebot_plugin_htmlrender").text_to_pic
+
+
+@text2pic.handle()
+async def _text2pic(bot: Bot, event: MessageEvent):
+    msg = str(event.get_message())
+
+    # css_path 可选
+    from pathlib import Path
+
+    pic = await text_to_pic(
+        text=msg, css_path=str(Path(__file__).parent / "templates" / "markdown.css")
+    )
+
+    pic = await text_to_pic(text=msg)
+    a = Image.open(io.BytesIO(pic))
+    a.save("text2pic.png", format="PNG")
+    await text2pic.finish(MessageSegment.image(pic))
+
+
+# 加载本地 html 方法
+html2pic = on_command("html2pic")
+new_page = require("nonebot_plugin_htmlrender").get_new_page
+
+
+@html2pic.handle()
+async def _html2pic(bot: Bot, event: MessageEvent):
+    from pathlib import Path
+
+    # html 中需要包括样式表
+    async with new_page(viewport={"width": 300, "height": 300}) as page:
+        await page.goto(
+            "file://" + (str(Path(__file__).parent / "html2pic.html")),
+            wait_until="networkidle",
+        )
+        pic = await page.screenshot(full_page=True, path="./html2pic.png")
+
+    await html2pic.finish(MessageSegment.image(pic))
+
+
+# 使用 template2pic 加载模板
+template2pic = on_command("template2pic")
+template_to_html = require("nonebot_plugin_htmlrender").template_to_html
+html_to_pic = require("nonebot_plugin_htmlrender").html_to_pic
+
+
+@template2pic.handle()
+async def _template2pic(bot: Bot, event: MessageEvent):
+    from pathlib import Path
+
+    text_list = ["1", "2", "3", "4"]
+    # 设置模板
+    # 模板中需要包括样式表
+    html = await template_to_html(
+        template_path=str(Path(__file__).parent / "templates"),
+        template_name="text.html",
+        text_list=text_list,
+    )
+    # 渲染图片
+    pic = await html_to_pic(html, viewport={"width": 600, "height": 300})
+
+    a = Image.open(io.BytesIO(pic))
+    a.save("template2pic.png", format="PNG")
+
+    await template2pic.finish(MessageSegment.image(pic))
+
+
+# 使用 md2pic
+md2pic = on_command("md2pic")
+md_to_pic = require("nonebot_plugin_htmlrender").md_to_pic
+
+
+@md2pic.handle()
+async def _md2pic(bot: Bot, event: MessageEvent):
+    from pathlib import Path
+
+    msg = str(event.get_message())
+
+    # css_path 可选
+    # pic = await md_to_pic(
+    #     md=msg, css_path=str(Path(__file__).parent / "templates" / "markdown.css")
+    # )
+
+    pic = await md_to_pic(md=msg)
+
+    a = Image.open(io.BytesIO(pic))
+    a.save("md2pic.png", format="PNG")
+
+    await md2pic.finish(MessageSegment.image(pic))
--- nonebot_plugin_htmlrender/date_source.py
+++ nonebot_plugin_htmlrender/date_source.py
@@ -1,17 +1,16 @@
 from jinja2.environment import Template
 from .browser import get_new_page
 from pathlib import Path
-from typing import Optional, ParamSpecArgs, Union
 import jinja2
 from nonebot.log import logger
 import markdown
 import aiofiles
 
-templates_path = Path(__file__).parent / "templates"
+TEMPLATES_PATH = str(Path(__file__).parent / "templates")
 
 env = jinja2.Environment(
     extensions=["jinja2.ext.loopcontrols"],
-    loader=jinja2.FileSystemLoader(Path(__file__).parent / "templates"),
+    loader=jinja2.FileSystemLoader(TEMPLATES_PATH),
     enable_async=True,
 )
 
@@ -25,57 +24,114 @@ async def create_image(html: str, wait: int = 0) -> bytes:
     return img_raw
 
 
-async def text_to_pic(text: str, css: Union[Path, str] = None) -> bytes:
+async def text_to_pic(text: str, css_path: str = None) -> bytes:
     """多行文本转图片
 
     Args:
         text (str): 纯文本, 可多行
-        css (Union[Path,str]): css文件路径或css文本
+        css_path (str, optional): css文件
 
     Returns:
         bytes: 图片, 可直接发送
     """
-
     template = env.get_template("text.html")
     text_list = text.split("\n")
 
     return await create_image(
         await template.render_async(
-            text_list=text_list, css=read_css(css) if css else None
+            text_list=text_list,
+            css=await read_file(css_path)
+            if css_path
+            else await read_file(TEMPLATES_PATH + "/text.css"),
         )
     )
 
 
-async def md_to_pic(md: Union[Path, str], css: Union[Path, str] = None) -> bytes:
+async def md_to_pic(md: str = None, md_path: str = None, css_path: str = None) -> bytes:
     """markdown 转 图片
 
     Args:
-        md (Union[Path, str]): markdown文件路径 或 markdown 格式文本
-        css (Union[Path, str], optional): css文件路径 或 css 文本. Defaults to None.
+        md (str, optional): markdown 文件路径 或 markdown 格式文本
+        md_path (str, optional): markdown 文件路径
+        css_path (str,  optional): css文件路径. Defaults to None.
 
     Returns:
         bytes: 图片, 可直接发送
     """
     template = env.get_template("markdown.html")
+    if md:
+        return await create_image(
+            await template.render_async(
+                md=markdown.markdown(md),
+                css=await read_file(css_path)
+                if css_path
+                else await read_file(TEMPLATES_PATH + "/markdown.css"),
+            )
+        )
 
-    return await create_image(
-        await template.render_async(md=read_md(md), css=read_css(css) if css else None)
-    )
+    elif md_path:
+        md = markdown.markdown(await read_file(md_path))
+        return await create_image(
+            await template.render_async(
+                md=md,
+                css=await read_file(css_path)
+                if css_path
+                else await read_file(TEMPLATES_PATH + "/markdown.css"),
+            ),
+        )
 
-
-async def read_css(css: Union[str, Path]) -> Optional[str]:
-    if type(css) == str:
-        return css
-    elif type(css) == Path:
-        async with aiofiles.open(str(css.resolve()), mode="r") as f:
-            return await f.read()
     else:
-        return None
+        raise "必须输入 md 或 md_path"
 
 
-async def read_md(md: Union[str, Path]) -> str:
-    if type(md) == str:
-        return md
-    elif type(md) == Path:
-        async with aiofiles.open(str(md.resolve()), mode="r") as f:
-            return await f.read()
+# async def read_md(md_path: str) -> str:
+#     async with aiofiles.open(str(Path(md_path).resolve()), mode="r") as f:
+#         md = await f.read()
+#     return markdown.markdown(md)
+
+
+async def read_file(path: str) -> str:
+    async with aiofiles.open(path, mode="r") as f:
+        return await f.read()
+
+
+async def template_to_html(
+    template_path: str,
+    template_name: str,
+    **kwargs,
+) -> str:
+    """使用jinja2模板引擎通过html生成图片
+
+    Args:
+        template_path (str): 模板路径
+        template_name (str): 模板名
+        **kwargs: 模板内容
+    Returns:
+        str: html
+    """
+
+    template_env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(template_path),
+        enable_async=True,
+    )
+    template = template_env.get_template(template_name)
+
+    return await template.render_async(**kwargs)
+
+
+async def html_to_pic(html: str, wait: int = 10, **kwargs) -> bytes:
+    """html转图片
+
+    Args:
+        html (str): html文本
+        wait (int, optional): 等待时间. Defaults to 10.
+
+    Returns:
+        bytes: 图片, 可直接发送
+    """
+    logger.debug(f"html:\n{html}")
+    async with get_new_page(**kwargs) as page:
+        await page.set_content(html, wait_until="networkidle")
+        await page.wait_for_timeout(wait)
+        img_raw = await page.screenshot(full_page=True)
+    return img_raw
