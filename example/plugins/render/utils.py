b1caa8f3df8d8614d1c3ab6a48087be0ca36c925
KomoriDev <mute231010@gmail.com>
2024-08-18 22:47:47 +0800

    :sparkles: 支持自定义过滤器

---
+++ example/plugins/render/utils.py
@@ -0,0 +1,12 @@
+from nonebot.log import logger
+
+def count_to_color(count: str) -> str:
+    logger.debug(f"Filtering count: {count}") 
+    if count == '1':
+        return "#facc15"
+    elif count == '2':
+        return "#f87171"
+    elif count == '3':
+        return "#c084fc"
+    else:
+        return "#60a5fa"
