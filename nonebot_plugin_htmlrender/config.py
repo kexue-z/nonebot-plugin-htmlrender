8d1808ab3a86f3e5f250e6affd7efb02e764def2
BalconyJH <balconyjh@gmail.com>
2024-12-04 21:46:45 +0800

    :recycle: 重构browser.py，增加镜像优选，调整playwright依赖安装

--- nonebot_plugin_htmlrender/config.py
+++ nonebot_plugin_htmlrender/config.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from pydantic import BaseModel, Field
+from nonebot import get_driver, get_plugin_config
 
 
 class Config(BaseModel):
@@ -10,3 +11,7 @@ class Config(BaseModel):
     htmlrender_browser_channel: Optional[str] = Field(default=None)
     htmlrender_browser_executable_path: Optional[str] = Field(default=None)
     htmlrender_connect_over_cdp: Optional[str] = Field(default=None)
+
+
+global_config = get_driver().config
+plugin_config = get_plugin_config(Config)
