24f81ba162783a1c6eb4393e8023c96a17bb048a
BalconyJH <balconyjh@gmail.com>
2025-01-14 23:26:42 +0800

    :sparkles: Use nonebot compatibility layer to verify browser type configuration items in advance

--- nonebot_plugin_htmlrender/config.py
+++ nonebot_plugin_htmlrender/config.py
@@ -1,11 +1,30 @@
-from typing import Optional
+from collections.abc import Generator
+from typing import Any, Callable, ClassVar, Optional
 
-from pydantic import BaseModel, Field
 from nonebot import get_driver, get_plugin_config
+from nonebot.compat import custom_validation
+from pydantic import BaseModel, Field
+
+
+@custom_validation
+class BrowserEngineType(str):
+    ALLOWED_VALUES: ClassVar = ["chromium", "firefox", "webkit"]
+
+    @classmethod
+    def __get_validators__(cls) -> Generator[Callable[..., Any], None, None]:
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, value: Any) -> str:
+        if value not in cls.ALLOWED_VALUES:
+            raise ValueError(
+                f"Invalid browser type: {value!r}, must be one of {cls.ALLOWED_VALUES}"
+            )
+        return value
 
 
 class Config(BaseModel):
-    htmlrender_browser: Optional[str] = Field(default="chromium")
+    htmlrender_browser: BrowserEngineType = Field(default="chromium")
     htmlrender_download_host: Optional[str] = Field(default=None)
     htmlrender_proxy_host: Optional[str] = Field(default=None)
     htmlrender_browser_channel: Optional[str] = Field(default=None)
