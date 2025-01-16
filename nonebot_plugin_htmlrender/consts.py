9210c0a90b77b63e92dfa46bb928755a5a0b5694
BalconyJH <balconyjh@gmail.com>
2025-01-16 23:37:28 +0800

    :truck: Move MIRRORS to consts

--- nonebot_plugin_htmlrender/consts.py
+++ nonebot_plugin_htmlrender/consts.py
@@ -1,5 +1,14 @@
+from dataclasses import dataclass
 import os
 import sys
+
+
+@dataclass(frozen=True)
+class MirrorSource:
+    name: str
+    url: str
+    priority: int
+
 
 # consts
 PLUGINS_GROUP = "nonebot_plugin_htmlrender"
@@ -9,3 +18,7 @@ REQUIRES_PYTHON = (3, 9)
 WINDOWS = sys.platform.startswith("win") or (sys.platform == "cli" and os.name == "nt")
 # MINGW = sysconfig.get_platform().startswith("mingw")
 # MACOS = sys.platform == "darwin"
+MIRRORS = [
+    MirrorSource("官方", "https://playwright.azureedge.net/builds/", 0),
+    # MirrorSource("淘宝", "https://npmmirror.com/mirrors/playwright", 1),
+]
